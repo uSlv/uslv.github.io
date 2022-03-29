5d8edc5c2c06e0c4a7614008ddb08bae3566ff40
Gabriel Barros <76067595+uSlv@users.noreply.github.com>
2022-03-29 16:24:16 -0300

    Add files via upload

---
+++ js/owl/main.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    smartSpeed: 1500,
+    autoplayHoverPause: true,
+})
+
